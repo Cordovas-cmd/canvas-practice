9b365615ec97fef7a5c1c2ab79f1f94da59f29fa
Samuel Cordova <76268942+Cordovas-cmd@users.noreply.github.com>
2025-07-23 13:17:32 -0500

    added some basic control checks for wasd keys on the browser for sprite to move around

--- index.js
+++ index.js
@@ -11,7 +11,29 @@ player.src="./sprites/Illidian-Idle(3).png"
 var x = 0;
 
 var y = 10;
+
+var key = 0;
 player.onload = function() {
     ctx.drawImage(player, x, y)
+};
+
+document.onkeydown = function(e) {
+    key = e.key }
+
+setInterval(function () {
+if(key === "w") {
+    y -= 1;
 }
-;
+if(key === "s") {
+    y += 1;
+}
+if(key === "a") {
+    x -= 1;
+}
+if(key === "d") {
+    x += 1;
+}
+ctx.clearRect(0, 0, 2000, 2000);
+ctx.drawImage(player, x, y)
+
+}, 5);
