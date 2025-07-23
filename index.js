ea8a9e5e60f076bb91cf17d2860747fdf7917283
Samuel Cordova <76268942+Cordovas-cmd@users.noreply.github.com>
2025-07-23 13:27:27 -0500

    added the checks to check for keyup and stop player movement

--- index.js
+++ index.js
@@ -12,15 +12,23 @@ var x = 0;
 
 var y = 10;
 
-var key = 0;
+var key, keyState = 0;
 player.onload = function() {
     ctx.drawImage(player, x, y)
 };
 
 document.onkeydown = function(e) {
+    keyState = "keydown"
     key = e.key }
 
+    document.onkeyup = function(e) {
+    keyState = "keyup";
+}
 setInterval(function () {
+
+    if(keyState === "keyup"){
+        return;
+    }
 if(key === "w") {
     y -= 1;
 }
@@ -33,7 +41,10 @@ if(key === "a") {
 if(key === "d") {
     x += 1;
 }
+
+
 ctx.clearRect(0, 0, 2000, 2000);
 ctx.drawImage(player, x, y)
 
 }, 5);
+
